7488df494565714e08b6b567d1898cadbda90552
amarjyothi98 <amarjyothi526@gmail.com>
2023-04-10 10:08:25 +0530

    finished the giphy process

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
 import ReactGiphySearchbox from 'react-giphy-searchbox'
-
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
+    <header>
+      <h1>React giphy searchbox</h1>
+    </header>
+    <div className="searchboxWrapper">
+      <ReactGiphySearchbox
+        apiKey='w7mZuyDd1v6lVfM6WyzuAdynkLYMac7H'
+        // onSelect={(item) => console.log(item);}
+        masonryConfig={[
+          {columns: 2, imageWidth:110, gutter:5},
+          {mq:"700px", columns:3, imageWidth: 120, gutter: 5}
+        ]}
+      />
     </div>
+    </>
   );
 }
 
